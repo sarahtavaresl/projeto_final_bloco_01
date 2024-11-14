9af2d9df731c8d6c3c7fddc2b683fcab5ffa0230
Sarah Tavares de Lima <sarahtavarez.lima@gmail.com>
2024-11-14 14:20:43 -0300

    Enviando Menu

---
+++ src/loja/Menu.java
@@ -0,0 +1,17 @@
+package loja;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 55319
+ *
+ */
+module projeto_final_bloco_01 {
+}
